d681b3692ee83747a346173111f1dbb8d1af3d70
mayankrd <mayank.dyl@gmail.com>
2017-08-01 18:53:33 -0400

    updated server demo

--- server.js
+++ server.js
@@ -13,13 +13,8 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
-
-
 // Point static path to dist -- For building -- REMOVE
 app.use(express.static(path.join(__dirname, 'dist')));
-
-
 
 // CORS
 app.use(function(req, res, next) {
